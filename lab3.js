6c4e209045e2af9223cbe394db0d5d31e39e6ecc
thetstone1 <m186252@usna.edu>
2018-03-08 00:14:21 -0500

    my webpage

---
+++ lab3.js
@@ -0,0 +1,8 @@
+function paradise() {
+
+window.alert("Warning: Hide Your American Express Before Watching this Video!");
+window.location.href = ("https://www.youtube.com/watch?v=FHCYHldJi_g&feature=player_embedded");
+document.getElementById("iframe").stop();
+}
+
+
